5bb3c143440c6faf191fee84acabe3b0ec6ed019
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-13 02:25:44 +0000

    first commit

---
+++ lib/movies_opening.rb
@@ -0,0 +1,6 @@
+require "movies_opening/version"
+
+module MoviesOpening
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/movies_opening/version.rb
@@ -0,0 +1,3 @@
+module MoviesOpening
+  VERSION = "0.1.0"
+end
