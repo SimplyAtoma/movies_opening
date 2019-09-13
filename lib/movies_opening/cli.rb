d1e9a33167970049544536f70bc667a04efab2eb
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-13 19:50:22 +0000

    created movies class

--- lib/movies_opening/cli.rb
+++ lib/movies_opening/cli.rb
@@ -3,14 +3,35 @@ class MoviesOpening::CLI
   def call 
     list_movies
     menu
+    goodbye
   end 
   
   def list_movies
      puts "Movies playing today: "
+     MoviesOpening::Movies.today
   end 
   
   def menu
-    puts "Enter the number of the movie you would like more info about: "
-    input = gets.strip
+    input = nil
+    while input != "exit"
+    puts "Enter the number of the movie you would like more info about, or type list to see the list again, or type exit: "
+      input = gets.strip.downcase
+      case input
+      when "1"
+        
+      when "2"
+        
+      when "3"
+        
+      when "list"
+        list_movies
+      else 
+        puts "Not sure what you want, type list or exit."
+      end 
+    end
   end 
+  
+  def goodbye
+    puts "See you tomorrow for more movie times!!"
+  end
 end
---
+++ lib/movies_opening/movies.rb
@@ -0,0 +1,3 @@
+class Movies
+  
+end
