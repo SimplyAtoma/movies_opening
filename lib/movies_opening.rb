22fca9ab1e5b8c470389b11c19aeef58a74a31f6
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-14 04:09:42 +0000

    added nokogiri

--- lib/movies_opening.rb
+++ lib/movies_opening.rb
@@ -1,3 +1,4 @@
+require "nokogiri"
 require_relative "./movies_opening/version"
 require_relative "./movies_opening/movies"
 require_relative "./movies_opening/cli"
--- lib/movies_opening/movies.rb
+++ lib/movies_opening/movies.rb
@@ -1,11 +1,12 @@
 class MoviesOpening::Movies
   attr_accessor :name, :time, :price, :available, :url
   def self.today
-    movie_1 = self.new
-    movie_1.name #name of movie 
-    movie_1.time #time playing 
-    movie_1.price #price of ticket
-    movie_1.available #if the tickets are available
-    movie_1.url 
+     self.scrape_movies
   end
+  
+  def self.scrape_movies
+    movies = []
+    #go to fandango
+    #go to area code 
+    #extract properties
 end
