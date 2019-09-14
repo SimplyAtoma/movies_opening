45a5ca7adc5462f91e72ba74825471b95c668be9
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-14 21:27:02 +0000

    scrape method cleanup

--- lib/movies_opening/movies.rb
+++ lib/movies_opening/movies.rb
@@ -18,10 +18,10 @@ class MoviesOpening::Movies
     url = "https://www.fandango.com/movies-in-theaters"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    movie_cards = parsed_page.css("div.poster-card")
+    movie_cards = parsed_page.css("div.poster-card--title-block")
     movie_cards.each do |movie_card|
       movie = Movies.new
-      movie.name = first_movie.css("span.title").text
+      movie.name = first_movie.css("span.heading-style-1 browse-movielist--title poster-card--title").text
       movie.url = first_movie.css('a')[0].attributes["href"].value 
       movielist << movie
     end
