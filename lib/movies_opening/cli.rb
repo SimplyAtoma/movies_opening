38e4533678b9566dc03688105068f303c263d4a1
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-14 03:54:28 +0000

    cleaned up cli code

--- lib/movies_opening/cli.rb
+++ lib/movies_opening/cli.rb
@@ -8,7 +8,10 @@ class MoviesOpening::CLI
   
   def list_movies
      puts "Movies playing today: "
-     MoviesOpening::Movies.today
+     @movies = MoviesOpening::Movies.today
+     @movies.each.with_index(1) do |movie, i|
+       puts "#{i}. #{movie.name} - plays at #{movie.time} - #{movie.price} per ticket - #{movie.available}\n"
+     end
   end 
   
   def menu
@@ -16,14 +19,10 @@ class MoviesOpening::CLI
     while input != "exit"
     puts "Enter the number of the movie you would like more info about, or type list to see the list again, or type exit: "
       input = gets.strip.downcase
-      case input
-      when "1"
-        
-      when "2"
-        
-      when "3"
-        
-      when "list"
+      if input.to_i > 0
+        the_movie = @movies[input.to_i-1]
+        puts "#{the_movie.name} - plays at #{the_movie.time} - #{the_movie.price} per ticket - #{the_movie.available}\n"
+      elsif input == "list"
         list_movies
       else 
         puts "Not sure what you want, type list or exit."
--- lib/movies_opening/movies.rb
+++ lib/movies_opening/movies.rb
@@ -1,5 +1,11 @@
 class MoviesOpening::Movies
+  attr_accessor :name, :time, :price, :available, :url
   def self.today
-    
+    movie_1 = self.new
+    movie_1.name #name of movie 
+    movie_1.time #time playing 
+    movie_1.price #price of ticket
+    movie_1.available #if the tickets are available
+    movie_1.url 
   end
 end
