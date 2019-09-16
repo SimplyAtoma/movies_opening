048c2abab34173679a493d212f6d14502323ae8a
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-16 02:33:19 +0000

    added comments

--- lib/movies_opening/movies.rb
+++ lib/movies_opening/movies.rb
@@ -6,17 +6,18 @@ class MoviesOpening::Movies
   
   def self.scrape_movies
     movies = []
-    #go to fandango
-    #go to area code 
+    #goes to fandango
     #extract properties
     movies = self.scrape_fandango
     movies
   end 
   
-  def self.scrape_fandango 
+  def self.scrape_fandango
+    #this method is a scrapper method that goes to fandango.com
     movielist = []
     parsed_page = Nokogiri::HTML(open("https://www.fandango.com"))
-    movie_cards = parsed_page.search("div.fluid.poster")
+    movie_cards = parsed_page.search("div.fluid.poster") #this line goes through the page and finds all the poster cards
+    #which is then assigned to movie_cards to hold them all  
     movie_cards.each do |movie_card|
       movie = self.new
       movie.name = movie_card.search("a.heading-style-1.heading-size-s.heading__movie-carousel")[0].text
@@ -24,6 +25,7 @@ class MoviesOpening::Movies
       movie.price = "$10.00"
       movie.url = movie_card.search('a')[0].attributes["href"].value
       movielist << movie
+      #the loop goes through the array makes a movie and assigns all the values of the name, day starts playing, price and url 
     end
     movielist
   end
